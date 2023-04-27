b107419681af98ce84e4cbecb7a9394053952e97
Ly Quoc Binh <53855738+TheHellhand@users.noreply.github.com>
2023-04-27 19:45:30 +0700

    Update dynamic game speed

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
 extern crate piston_window;
 
-mod utils;
 mod snake;
+mod utils;
 
-use rand::Rng;
-use piston_window::*;
-use crate::snake::Game;
 use crate::snake::Bait;
+use crate::snake::Game;
 use crate::snake::Snake;
 use crate::snake::SnakeDirection;
+use piston_window::*;
+use rand::Rng;
 
 fn main() {
-    let window_width: u32 = 500;
-    let window_height: u32 = 500;
-
+    let window_dimensions = [500, 500];
     let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [window_width, window_height])
+        WindowSettings::new("Hello Piston!", window_dimensions)
             .exit_on_esc(true)
             .build()
             .unwrap();
 
+    let mut game: Game = Game::new(1.0, 0.0);
+
     const SNAKE_WIDTH: f64 = snake::GRID_UNIT;
     const SNAKE_HEIGHT: f64 = snake::GRID_UNIT;
-    let mut main_snake: Snake = Snake { 
-        value: [0.0, 0.0, SNAKE_WIDTH, SNAKE_HEIGHT], 
+    let mut main_snake: Snake = Snake {
+        value: [0.0, 0.0, SNAKE_WIDTH, SNAKE_HEIGHT],
         color: [0.0, 0.0, 0.0, 1.0],
-        direction: SnakeDirection::Right
+        direction: SnakeDirection::Right,
     };
 
     const BAIT_WIDTH: f64 = snake::GRID_UNIT;
     const BAIT_HEIGHT: f64 = snake::GRID_UNIT;
-    let mut bait: Bait = Bait { 
-        value: [5.0 * snake::GRID_UNIT, 5.0 * snake::GRID_UNIT, BAIT_WIDTH, BAIT_HEIGHT], 
-        color: [1.0, 0.0, 0.0, 1.0]
+    let mut bait: Bait = Bait {
+        value: [
+            5.0 * snake::GRID_UNIT,
+            5.0 * snake::GRID_UNIT,
+            BAIT_WIDTH,
+            BAIT_HEIGHT,
+        ],
+        color: [1.0, 0.0, 0.0, 1.0],
     };
-
-    let mut game: Game = Game::new(1.0, 0.0);
 
     while let Some(e) = window.next() {
         if let Event::Input(input, _) = &e {
@@ -64,14 +67,24 @@ fn main() {
                 println!("Score: {}", &game.score);
             }
         }
-        // time_weight = utils::get_time_weight();
+        game.add_tick();
 
         window.draw_2d(&e, |context, graphics, _device| {
             clear([1.0; 4], graphics);
-            rectangle(main_snake.color, main_snake.value, context.transform, graphics);
+            rectangle(
+                main_snake.color,
+                main_snake.value,
+                context.transform,
+                graphics,
+            );
             if &main_snake.value == &bait.value {
                 let random: f64 = rand::thread_rng().gen_range(0..10) as f64;
-                bait.value = [random * snake::GRID_UNIT, random * snake::GRID_UNIT, BAIT_WIDTH, BAIT_HEIGHT];
+                bait.value = [
+                    random * snake::GRID_UNIT,
+                    random * snake::GRID_UNIT,
+                    BAIT_WIDTH,
+                    BAIT_HEIGHT,
+                ];
                 rectangle(bait.color, bait.value, context.transform, graphics);
             } else {
                 rectangle(bait.color, bait.value, context.transform, graphics);
--- src/snake.rs
+++ src/snake.rs
@@ -1,22 +1,35 @@
 pub const GRID_UNIT: f64 = 20.0;
 
 pub struct Game {
-    pub speed: f32,
+    pub speed: f64,
     pub score: f32,
+    curr_move_weight: f64,
     ready_to_move: bool
 }
 
 impl Game {
-    pub fn new(speed: f32, score: f32) -> Game {
+    pub fn new(speed: f64, score: f32) -> Game {
         Game {
             speed,
             score,
+            curr_move_weight: 0.0,
             ready_to_move: true
         }
     }
 
     pub fn should_move(&self) -> bool {
         self.ready_to_move
+    }
+
+    pub fn add_tick(&mut self) {
+        if self.ready_to_move == true {
+            self.ready_to_move = false; 
+        }
+        self.curr_move_weight += self.speed * 0.01;
+        if self.curr_move_weight >= 1.0 {
+            self.curr_move_weight = 0.0;
+            self.ready_to_move = true;
+        }
     }
 }
 
